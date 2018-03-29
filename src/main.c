903324409188e0dc132c73a2e065db02417d81ca
Jaroslav Beran <jaroslav.beran@gmail.com>
2018-03-29 16:49:20 +0200

    Refactoring

--- src/main.c
+++ src/main.c
@@ -13,12 +13,7 @@
 #define SCREEN_WIDTH 32
 
 
-typedef struct {
-        int id;
-        char *description;
-        char *ways;
-} ROOM;
-
+/* way */
 typedef struct {
         char description[BLEN];
         char key[BLEN];
@@ -26,6 +21,14 @@ typedef struct {
         char has_to_have_item;
         char can_get_item;
 } OPTION;
+
+
+/* room */
+typedef struct {
+        int id;                         /* room id */
+        char *description;              /* room description */
+        char *ways;                     /* list of ways from room */
+} ROOM;
 
 
 char items[BLEN];
@@ -50,68 +53,103 @@ ROOM map[] = {
         
 
 
-static int getOption(char *opts);
+void getOptions(OPTION **o, int *size, char *opts);
 static void myprint(char *s);
 static void insert_item(char c);
 
 
 int main(void) {
-        
+        char buff[BLEN];
+        OPTION *o;
+        int size, i, c, choice;
         items[0] = '\0';
-        
+
         while (1) {
                 printf("\n\n%s\n\n", name);
                 myprint(map[croom].description);
                 
                 printf("\n\n\n%s", "Muzes:\n");
-                croom = getOption(map[croom].ways); 
+                
+                /* print options */
+                getOptions(&o, &size, map[croom].ways);
+                buff[0] = '\0';
+                for (i = 0; i < size; i++) {
+                        if ((o[i].has_to_have_item == '\0' && o[i].can_get_item == '\0') 
+                                || (o[i].has_to_have_item == '\0' && strchr(items, o[i].can_get_item) == NULL)
+                                || (o[i].has_to_have_item != '\0' && strchr(items, o[i].has_to_have_item) != NULL)
+                         ) {
+                                strncat(buff, o[i].description, BLEN);
+                                strncat(buff, " ", BLEN);
+                        }
+                }
+
+                myprint(buff);
+                printf("\n\n>");
+
+                /* get choice */
+                choice = 0;
+                while (choice <= 0) {
+                        c = getchar();
+                        for (i = 0; i < size; i++) {
+                                if (strchr(o[i].key, c) != NULL) {
+                                        choice = o[i].room;
+                                        if (o[i].can_get_item != '\0') {
+                                                insert_item(o[i].can_get_item);        
+                                        }
+                                        break;
+                                }
+                        }
+                }
+                
+                free((void *) o);
+                croom = choice; 
         }
         return 0;
 }
 
-int getOption(char *opts) {
-        char buff[BLEN], buff2[BLEN], *p;
-        int size, x, y, c, result;
-        OPTION *o;
-        
-        size = 0;
+
+void getOptions(OPTION **o, int *size, char *opts) {
+        char buff[BLEN], *p;
+        int x, y;
+
+        *size = 0;
         p = opts;
         while (*p != '\0') {
-                if (*p == ';') size++;
+                if (*p == ';') (*size)++;
                 p++;
         }
-        
-        o = (OPTION *) malloc(sizeof(OPTION) * size);
-        if (o == NULL) {
+
+        *o = (OPTION *) malloc(sizeof(OPTION) * (*size));
+        if (*o == NULL) {
                 printf("FATAL: Cannot allocate memory\n");
                 exit(0);
         }
-        
+
         x = 0;
         y = 0;
         buff[0] = '\0';
         p = opts;
-        
+
         while (*p != '\0') {
-        
+
                 if (*p == ';') {
-                        o[y].can_get_item = *buff;
+                        (*o)[y].can_get_item = *buff;
                         x = 0;
                         y++;
                         buff[0] = '\0';
                         p++;
                         continue;
                 }
-                
+
                 if (*p == ',') {
                         switch (x) {
-                                case 0: strcpy(o[y].description, buff);
+                                case 0: strcpy((*o)[y].description, buff);
                                         break;
-                                case 1: strcpy(o[y].key, buff);
+                                case 1: strcpy((*o)[y].key, buff);
                                         break;
-                                case 2: o[y].room = atoi(buff);
+                                case 2: (*o)[y].room = atoi(buff);
                                         break;
-                                case 3: o[y].has_to_have_item = *buff;
+                                case 3: (*o)[y].has_to_have_item = *buff;
                                         break; 
                         }
                         x++;
@@ -119,59 +157,29 @@ int getOption(char *opts) {
                         p++;
                         continue;
                 }
-                
+
                 if (strlen(buff) + 1 == BLEN) {
                         p++;
                         continue;
                 }
-                 
+
                 buff[strlen(buff) + 1] = '\0';
                 buff[strlen(buff)] = *p; 
                 p++;
         }
-        
-        buff2[0] = '\0';
-        
-        for (y = 0; y < size; y++) {
-                if ((o[y].has_to_have_item == '\0' && o[y].can_get_item == '\0') 
-                 || (o[y].has_to_have_item == '\0' && strchr(items, o[y].can_get_item) == NULL)
-                 || (o[y].has_to_have_item != '\0' && strchr(items, o[y].has_to_have_item) != NULL)
-                 ) {
-                        strncat(buff2, o[y].description, BLEN);
-                        strncat(buff2, " ", BLEN);
-                }
-        }
-        
-        myprint(buff2);
-        printf("\n\n>");
-        
-        result = 0;
-        while (result <= 0) {
-                c = getchar();
-                for (y = 0; y < size; y++) {
-                        if (strchr(o[y].key, c) != NULL) {
-                                result = o[y].room;
-                                if (o[y].can_get_item != '\0') {
-                                        insert_item(o[y].can_get_item);        
-                                }
-                                break;
-                        }
-                }
-        }
-        
-        free((void *) o);
-        return result;
+
+        return;
 }
 
 
 void myprint(char *s) {
         int i;
         char *p, *p1;
-        
+
         i = 0;
         p = s;
         p1 = p;
-        
+
         while (*p != '\0') {
                 
                 p = p1;
@@ -188,7 +196,7 @@ void myprint(char *s) {
                 if (*p1 == '\n') {
                         i = p1 - p;
                 }
-                
+
                 while (p <= p1 && *p != '\0') {
                         putchar(*p);
                         p++;
@@ -205,9 +213,8 @@ void insert_item(char c) {
                 if (strlen(items) + 1 < BLEN) {
                         p = items + strlen(items);
                         *p = c;
-                        p++;                        
-                        *p = '\0';                        
-                        
+                        p++;
+                        *p = '\0';
                 } 
         }
 }
