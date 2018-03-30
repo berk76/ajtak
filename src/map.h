6d8c0a15b74985ac978e9be31c192f8f0d823114
Jaroslav Beran <jaroslav.beran@gmail.com>
2018-03-30 23:01:00 +0200

    Automat

--- src/map.h
+++ src/map.h
@@ -25,27 +25,42 @@ ROOM map[] = {
             ""
             },
         {1, "Sedis u sveho pracovniho stolu a ospale koukas do monitoru. Naproti pres stul bezi vetrak. Venku je 30 stupnu.", 
-            "Jit na (k)kafe,Kk,2,,,;"
-            "Jit na (z)achod,Zz,3,,,;"
-            "Prohledat (s)uplik,Ss,5,,,;"
+            "Jit na (k)kafe.,Kk,2,,,;"
+            "Jit na (z)achod.,Zz,3,,,;"
+            "Jit si neco koupit do (a)utomatu na sklad.,Aa,7,,,;"
+            "Prohledat (s)uplik.,Ss,5,,,;"
+            "Jit pokecat s (B)oukym.,Bb,8,h,,;"
             },
         {2, "Jsi v kuchynce a popijis kafe z kavovaru AdrianoDesign. V umyvadle je hromada spinaveho nadobi. V rohu jsou zamcene dvere.",
-            "Jit na (z)patky k pocitaci,Zz,1,,,;"
-            "Precist si (i)nternert na mobilu,Ii,4,,,;"
-            "(O)demknout dvere a vejit,Oo,6,k,,;"
+            "Jit na (z)patky k pocitaci.,Zz,1,,,;"
+            "Precist si (i)nternert na mobilu.,Ii,4,,,;"
+            "(O)demknout dvere a vejit.,Oo,6,k,,;"
             },
         {3, "Jsi na zachode.",
-            "Jit na (z)patky k pocitaci,Zz,1,,,;"
+            "Jit na (z)patky k pocitaci.,Zz,1,,,;"
             },
         {4, "Ctes si zpravicky:\n\n* V Recku parlament hlasuje o prijeti podminek pujcky\n\n* Masarykovo nadrazi je uzavreno, protoze vlak nedobrzdil.",
-            "(V)ypmout mobil a schovat ho do kapsy,Vv,2,,,;"
+            "(V)ypmout mobil a schovat ho do kapsy.,Vv,2,,,;"
             },
         {5, "V supliku je bordel.", 
-            "(V)zit klic,Vv,1,,k,V tom bordelu jsi nasel klic.;"
-            "(Z)avrit suplik,Zz,1,,,;"
+            "(V)zit klic.,Vv,1,,k,V tom bordelu jsi nasel klic.;"
+            "(Z)avrit suplik.,Zz,1,,,;"
             },
-        {6, "Jsi v uklidove komre. Je tu sero ale vidis kbelik, nejake hadry a dve kostata. ", 
-            "(J)it zpatky a zamknout,Jj,2,,,;"
+        {6, "Jsi v uklidove komre. Je tu sero ale vidis kbelik, nejake hadry a dve kostata.", 
+            "Jit (z)patky a zamknout.,Zz,2,,,;"
+            },
+        {7, "Stojis u automatu a premyslis co si koupit.",
+            "Koupit (h)oralku za osm.,Hh,1,,h,;"
+            "Koupit (c)oca-colu za patnact.,Cc,1,,c,;"
+            "Koupit (m)inonku za deset.,Mm,1,,m,;"
+            "Jit (z)patky.,Zz,1,,,;"
+            },
+        {8, "Stojis v kanclu u Boukyho, je tu trochu zatuchlo... Bouky nadava na webove sluzby a neni s nim rec.",
+            "Vzit Boukyho na (c)igo.,Cc,9,,,;"
+            "Jit (z)patky.,Zz,1,,,;"
+            },
+        {9, "Postavas s Boukym pred vchodem do budovy a poslouchas jak si stezuje na Blahu a na Sparktech. ",
+            "Jit (z)patky.,Zz,1,,,;"
             }
 };
 
